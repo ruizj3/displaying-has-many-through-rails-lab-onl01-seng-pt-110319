a8a1d507fc9874de42c979b55bea522311aa8a25
Joseph Ruiz <ruizj3@uw.edu>
2020-04-21 01:16:17 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200421004421) do
-
-  create_table "appointments", force: :cascade do |t|
-    t.datetime "appointment_datetime"
-    t.integer  "patient_id"
-    t.integer  "doctor_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
+ActiveRecord::Schema.define(version: 20200421004356) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
